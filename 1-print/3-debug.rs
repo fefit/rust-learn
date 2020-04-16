442519cada1cd6c9339262c771d69232118db967
ganmin <ganmin@360.cn>
2020-04-16 15:25:56 +0800

    增加变量声明的一些知识点

--- 1-print/3-debug.rs
+++ 1-print/3-debug.rs
@@ -1,21 +1,26 @@
 use std::fmt;
 
-struct Color{
+struct RGB{
   red: u8,
   green: u8,
   blue: u8
 }
-// 实现Display trait
-impl fmt::Display for Color{
+// 实现Display trait，以便可以通过{}格式化
+impl fmt::Display for RGB{
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
-    write!(f, "Color{{red:{},green:{},blue:{}}}", self.red, self.green, self.blue)
+    write!(f, "RGB{{red:{},green:{},blue:{}}}", self.red, self.green, self.blue)
   }
 }
-
-impl fmt::LowerHex for Color{
+// 实现LowerHex，以便可以通过{:x}格式化
+impl fmt::LowerHex for RGB{
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
     write!(f,"{:x}{:x}{:x}", self.red, self.green, self.blue)
   }
+}
+
+#[derive(Debug)]
+struct Shadow<T>{
+  bgcolor: T,
 }
 
 fn main(){
@@ -29,12 +34,15 @@ fn main(){
    * 针对相同特性（属性、字段）的事物，rust里可以通过结构体struct来表示。那么，如何来定义这些结构体能拥有哪些行为呢？
    * 这时候trait就派上用场了，trait有点像接口(interface)，它描述了一些方法，这些方法可能没有函数体内容、也可能包含着这个方法的默认实现，总之，可以为不同的数据类型实现这个trait里的方法，然后我们就可以通过以trait限定的方式声明某个变量，在变量上直接调用trait的方法了。
    */
-  let c = Color{
+  let c = RGB{
     red: 255,
     green: 255,
     blue: 255
   };
   // println!("{}", c);
-  // 如果没有生面的impl部分，上面注释的一行会报这样的错误：the trait `std::fmt::Display` is not implemented for `Color` 
+  // 如果没有生面的impl部分，上面注释的一行会报这样的错误：the trait `std::fmt::Display` is not implemented for `RGB` 
   println!("{:x}", c);
+  // 大部分类型我们可以通过实现fmt::Display trait来获得好的打印结果
+  // 但针对一些带泛型的类型，我们就不好去实现了，其数据类型可能是多样的，我们无法区分的打印出来所有不同类型的打印结果。
+  // 这时我们就需要用到fmt::Debug了，rust里通过过程宏#[derive(Debug)]就可以为类型自动添加实现。 
 }
